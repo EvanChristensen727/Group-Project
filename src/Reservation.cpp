b8bacf8d2743b9120c9612716bb7f9e55bb456a5
EvanChristensen727 <92061105+EvanChristensen727@users.noreply.github.com>
2026-09-10 15:41:00 -0500

    Update Reservation.cpp
    Created class definition for Reservation

--- src/Reservation.cpp
+++ src/Reservation.cpp
@@ -1,1 +1,18 @@
+using namespace stc;
 
+class Reservation{
+  public:
+    int get_reservation_ID();
+    int get_student_ID();
+    int get_student_name();
+    int get_resource_ID();
+    int get_date();
+    Reservation(int s_ID, string s_Name, string resource_id, string date, int reserve_ID);
+    Reservation(int s_ID, string s_Name, string resource_id, string date);
+  private:
+    int reservation_ID;
+    int student_ID;
+    string student_Name;  
+    string resource_ID;
+    string date;
+};
