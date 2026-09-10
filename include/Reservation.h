701d8f4349f31bc807d611f098d4dc5c20387396
EvanChristensen727 <92061105+EvanChristensen727@users.noreply.github.com>
2026-09-10 15:46:03 -0500

    Update Reservation.h

--- include/Reservation.h
+++ include/Reservation.h
@@ -1,1 +1,21 @@
+#ifndef RESERVATION_H
 
+#define RESERVATION_H
+class Reservation{
+  public: 
+    int get_reservation_ID();
+    int get_student_ID();
+    int get_student_name();
+    int get_resource_ID();
+    int get_date();
+    Reservation(int s_ID, string s_Name, string resource_id, string date, int reserve_ID);
+    Reservation(int s_ID, string s_Name, string resource_id, string date);
+    
+  private:
+    int reservation_ID;
+    int student_ID;
+    string student_Name;  
+    string resource_ID;
+    string date;
+
+#endif
