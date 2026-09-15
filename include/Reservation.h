16b403f5137406444600e6f66ac7af6bf1a2defc
EvanChristensen727 <92061105+EvanChristensen727@users.noreply.github.com>
2026-09-15 14:19:11 -0500

    Update Reservation.h
    Fixed the return types for the getter methods

--- include/Reservation.h
+++ include/Reservation.h
@@ -1,13 +1,15 @@
 #ifndef RESERVATION_H
 
 #define RESERVATION_H
+using namespace std;  
 class Reservation{
   public: 
     int get_reservation_ID();
     int get_student_ID();
-    int get_student_name();
-    int get_resource_ID();
-    int get_date();
+    string get_student_name();
+    string get_resource_ID();
+    string get_date();
+    void set_reservation_ID(int id);
     Reservation(int s_ID, string s_Name, string resource_id, string date, int reserve_ID);
     Reservation(int s_ID, string s_Name, string resource_id, string date);
     
