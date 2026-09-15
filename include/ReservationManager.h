d8e7e60c2d4e96cbc4f22622c3b7283cfbd89e8a
readyEddieN <edmnajera@gmail.com>
2026-09-15 14:07:21 -0500

    Created a basic header file for ReservationManager

---
+++ include/ReservationManager.h
@@ -0,0 +1,23 @@
+#ifndef RESERVATIONMANAGER_H
+#define RESERVATIONMANAGER_H
+    //resource -> reservation -> reservation manager (?)
+    #include <iostream>
+    #include <list>  // linked list for active reservation storage
+    #include <stack> // stack for cancellation history
+    #include <queue> // queue for placing reservations
+    #include "Reservation.h"
+    using namespace std;
+
+    class ReservationManager
+    {
+        public:
+            void CreateReservation();
+            void CancelReservation();
+            void UndoCancel();
+        private:
+            list<Reservation> currentReservs;
+            stack<Reservation> cancelledReservs;
+            queue<Reservation> waitingList;
+    }
+
+#endif
