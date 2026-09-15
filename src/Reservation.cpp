63947f9cf210a710d82722d8c3d1a072763ecf07
EvanChristensen727 <92061105+EvanChristensen727@users.noreply.github.com>
2026-09-15 14:40:28 -0500

    Update Reservation.cpp
    Added the ability to load a reservation from a string

--- src/Reservation.cpp
+++ src/Reservation.cpp
@@ -40,3 +40,19 @@ void Reservation::set_reservation_ID(int id){
   reservation_ID = id;
 }
 
+
+//Takes a line (string) in the same format as reservations.txt and returns the address of a new Reservation object 
+static Reservation Reservation::load_reservation(string line){
+  int i = 0;
+  string data[5] = ["", "", "", "", ""];
+  
+  for (char c : line){
+    if (c == "|"){
+      i++;
+      continue;
+    }
+    data[i] += c;
+  }
+  return new Reservation(stoi(data[1]), data[2], data[3], data[4], stoi(data[0]));
+}
+
