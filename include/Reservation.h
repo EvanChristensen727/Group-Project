99eccb12a5b6d1f6e24d93f1dd53bbf531081d3e
EvanChristensen727 <92061105+EvanChristensen727@users.noreply.github.com>
2026-09-15 14:45:19 -0500

    Update Reservation.h
    Changed from using a namespace to prevent pollution

--- include/Reservation.h
+++ include/Reservation.h
@@ -1,24 +1,24 @@
 #ifndef RESERVATION_H
 
 #define RESERVATION_H
-using namespace std;  
+
 class Reservation{
   public: 
     int get_reservation_ID();
     int get_student_ID();
-    string get_student_name();
-    string get_resource_ID();
-    string get_date();
+    std:string get_student_name();
+    std:string get_resource_ID();
+    std:string get_date();
     void set_reservation_ID(int id);
     static Reservation load_reservation(string line);
-    Reservation(int s_ID, string s_Name, string resource_id, string date, int reserve_ID);
-    Reservation(int s_ID, string s_Name, string resource_id, string date);
+    Reservation(int s_ID, std:string s_Name, std:string resource_id, std:string date, int reserve_ID);
+    Reservation(int s_ID, std:string s_Name, std:string resource_id, std:string date);
     
   private:
     int reservation_ID;
     int student_ID;
-    string student_Name;  
-    string resource_ID;
-    string date;
+    std:string student_Name;  
+    std:string resource_ID;
+    std:string date;
 };
 #endif
