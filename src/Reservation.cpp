dec614aa80b8d71bb7ffdbffde6d360f7cdd9554
Blair Rush <Blake.Rush2000@gmail.com>
2026-09-18 16:44:09 -0500

    corrected some of the errors in reservation.cpp

--- src/Reservation.cpp
+++ src/Reservation.cpp
@@ -1,22 +1,22 @@
 #include "Reservation.h"
 
-Reservation::Reservation(int s_ID, std:string s_Name, std:string resource_id, std:string date, int reserve_ID){
+Reservation::Reservation(int s_ID, std::string s_Name, std::string resource_id, std::string date, int reserve_ID){
   student_ID = s_ID;
   student_Name = s_Name;
   resource_ID = resource_id;
-  this->date* = date;
+  this->date = date;
   reservation_ID = reserve_ID;
 }
 
-Reservation::Reservation(int s_ID, std:string s_Name, std:string resource_id, std:string date){
+Reservation::Reservation(int s_ID, std::string s_Name, std::string resource_id, std::string date){
   student_ID = s_ID;
   student_Name = s_Name;
   resource_ID = resource_id;
-  this->date* = date;
+  this->date = date;
   reservation_ID = -1;
 }
 
-std:string Reservation::get_date(){
+std::string Reservation::get_date(){
   return date;
 }
 
@@ -28,11 +28,11 @@ int Reservation::get_student_ID(){
   return student_ID;
 }
 
-std:string Reservation::get_student_name(){
+std::string Reservation::get_student_name(){
   return student_Name;
 }
 
-std:string Reservation::get_resource_ID(){
+std::string Reservation::get_resource_ID(){
   return resource_ID;
 }
 
@@ -42,12 +42,12 @@ void Reservation::set_reservation_ID(int id){
 
 
 //Takes a line (string) in the same format as reservations.txt and returns the address of a new Reservation object 
-static Reservation Reservation::load_reservation(std:string line){
+static Reservation Reservation::load_reservation(std::string line){
   int i = 0;
-  std:string data[5] = ["", "", "", "", ""];
+  std::string data[5] = ["", "", "", "", ""];
   
   for (char c : line){
-    if (c == "|"){
+    if (c == '|'){
       i++;
       continue;
     }
