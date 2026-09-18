b454b94ec9b22245acf6563a565ff971886ba54b
readyEddieN <edmnajera@gmail.com>
2026-09-18 14:03:06 -0500

    Fleshed out Reservation Manager more.

--- include/ReservationManager.h
+++ include/ReservationManager.h
@@ -6,15 +6,17 @@
     #include <stack> // stack for cancellation history
     #include <queue> // queue for placing reservations
     #include "Reservation.h"
+
     using namespace std;
 
     class ReservationManager
     {
         public:
+            void SearchReservation(int id); //search by reservation id
+            void SearchReservation(string rId); //search by resource id
             void AddReservation(Reservation r); // for adding a new reservation
-            void CancelReservation();
+            void CancelReservation(int id);
             void UndoCancel();
-            void SearchReservation();
         private:
             list<Reservation> currentReservs;
             stack<Reservation> cancelledReservs;
--- src/ReservationManager.cpp
+++ src/ReservationManager.cpp
@@ -5,22 +5,46 @@ ReservationManager::ReservationManager()
 
 }
 
-void ReservationManager::AddReservation(Reservation r)
+void ReservationManager::SearchReservation(int id)
 {
-
+    //fuuuuck
 }
 
-void ReservationManager::CancelReservation()
+void ReservationManager::SearchReservation(string rId)
 {
+    //FUUUUUUCK
+}
 
+void ReservationManager::AddReservation(Reservation r)
+{
+    //TODO: check if the resource is available 
+
+    //check if it has an id
+    if (r.get_reservation_ID() == -1) 
+    {
+        if (currentReservs.empty()) //if this is the first reservation and there's no preexisting ID
+            r.set_reservation_ID(1);
+        else
+             r.set_reservation_ID(currentReservs.back().get_reservation_ID() + 1); //add 1 to the last reservation
+    }
+
+    //add it
+    currentReservs.push_back(r);
+}
+
+void ReservationManager::CancelReservation(int id)
+{
+    //search for id
 }
 
 void ReservationManager::UndoCancel()
 {
-
-}
-
-void ReservationManager::SearchReservation()
-{
-
+    if (cancelledReservs.empty())
+    {
+        cout << "No Reservations To Restore." << endl;
+        return;
+    }
+    currentReservs.push_back(cancelledReservs.top());
+    cancelledReservs.pop();
+    cout << "Reservation Restored Successfully." << endl;
 }
