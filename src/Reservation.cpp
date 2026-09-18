56d5bbfb567eb0564d8c8742e6458479e7e50837
Blair Rush <Blake.Rush2000@gmail.com>
2026-09-18 16:59:16 -0500

    more corrections

--- src/Reservation.cpp
+++ src/Reservation.cpp
@@ -42,9 +42,9 @@ void Reservation::set_reservation_ID(int id){
 
 
 //Takes a line (string) in the same format as reservations.txt and returns the address of a new Reservation object 
-static Reservation Reservation::load_reservation(std::string line){
+Reservation Reservation::load_reservation(std::string line){
   int i = 0;
-  std::string data[5] = ["", "", "", "", ""];
+  std::string data[5];
   
   for (char c : line){
     if (c == '|'){
@@ -53,6 +53,6 @@ static Reservation Reservation::load_reservation(std::string line){
     }
     data[i] += c;
   }
-  return new Reservation(stoi(data[1]), data[2], data[3], data[4], stoi(data[0]));
+  return Reservation(std::stoi(data[1]), data[2], data[3], data[4], std::stoi(data[0]));
 }
 
