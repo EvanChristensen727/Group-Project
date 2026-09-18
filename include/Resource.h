b0bd4312b01a67cd8da38534ef4bd3c3033b5467
Blair Rush <Blake.Rush2000@gmail.com>
2026-09-18 15:47:58 -0500

    added some comments for clarity

--- include/Resource.h
+++ include/Resource.h
@@ -34,9 +34,9 @@ class ResourceManager{
 		void displayResources();
 		//void searchResources(string criteria);
 		//void sortResources(string criteria); //not sure about this one yet
-		void setResourceAvailability(string r_ID, bool state);
-		bool resourceExists(string r_ID);
-		bool isAvailable(string r_ID);
+		void setResourceAvailability(string r_ID, bool state); // mutator that iterates throught the vector for ID, then changes state to provided state.
+		bool resourceExists(string r_ID); // accessor that iterates through the vector for a given string ID then returns if the ID exists
+		bool isAvailable(string r_ID);  //accessor that iterates through the vector for a given string ID then returns the state of the ID
 	private:
 	 	vector<Resource> Resource_List; 
 };
